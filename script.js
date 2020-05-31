3ed9bf0499a38a119d37afcd2e519d7910717480
lteachma <59899305+lteachma@users.noreply.github.com>
2020-05-31 19:38:45 -0400

    Add files via upload

---
+++ script.js
@@ -0,0 +1,9 @@
+var number = prompt("Input a Number")
+    
+    for(var i=prompt.answer; i<prompt.answer; i++){
+  writeAns(prompt.answer)
+};
+
+function writeAns(ans){
+  document.write(ans+" ")
+};
